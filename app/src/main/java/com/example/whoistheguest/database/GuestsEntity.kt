4f9b7c4f08afe1ef32be16e33f98308e15704d27
0HimanshuBansal <himanshubansal791999@gmail.com>
2022-02-04 15:20:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/whoistheguest/database/GuestsEntity.kt
@@ -0,0 +1,10 @@
+package com.example.whoistheguest.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "guests")
+data class GuestsEntity(
+
+)
